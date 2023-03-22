2835340ceab2cb50787995db28ddfc3e0b3a4524
Chris W <NA>
2023-03-22 17:31:02 +0000

    Comment unneeded code

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,22 +33,22 @@ def get_fruityvice_data(this_fruit_choice):
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
        back_from_function = get_fruityvice_data(fruit_choice)
        streamlit.dataframe(back_from_function)
-       
+
 except URLError as e:
     streamlit.error()
 
 ##streamlit.text(fruityvice_response.json())
 
-# Normalise JSON Data? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Display normalized data in dataframe?
-streamlit.dataframe(fruityvice_normalized)
+# # Normalise JSON Data? 
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# # Display normalized data in dataframe?
+# streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
 
 ##Query SF
